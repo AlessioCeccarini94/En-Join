08a12e3afca89aada76ad69e61e690cbd4bc1d5e
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-04-26 10:33:59 +0200

    added modal on aggiungi evento

--- EN-Join/src/app/components/hero-section/hero-section.ts
+++ EN-Join/src/app/components/hero-section/hero-section.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
-export class HeroSection {}
+export class HeroSection {
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+}
